307d5271785dc2b17642e8001894fa3a471c0a46
tanathorn_m <tanathorn.m@livingmobile.me>
2022-08-23 17:00:36 +0700

    add data validation json

---
+++ job/validation/validation_json_history_dividends_api.py
@@ -0,0 +1,4 @@
+import cerberus
+'''
+https://towardsdatascience.com/do-not-use-if-else-for-validating-data-objects-in-python-anymore-17203e1d65fe
+'''
--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from api import delisted_company_api
+from api import delisted_company_api, history_dividends_api
+from postgresql_function import psycopg2_postgresql
+import pandas as pd
+import pandera as p
+import dagster
+
+
+''' 
+Transform validation delisted_company 
+'''
+
+df_company = pd.json_normalize(delisted_company_api.get_jsonparsed_data_delisted_company(api_key='10c0c76dfaabef8d2ba52d1fb7e5bf8d'))
+df_company.to_csv('list.csv')
+print(df_company)
